62efb109a8dc69f5fb0a76fd21649b74f0d2a882
KiruthigaRagu <45934700+KiruthigaRagu@users.noreply.github.com>
2020-05-25 16:00:10 +0530

    Create Sharpening.py

---
+++ Filters in frequency domain/Sharpening.py
@@ -0,0 +1,36 @@
+#importing packages
+import cv2
+from google.colab.patches import cv2_imshow
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Read image
+img=cv2.imread('image.jpg',0)
+
+#transformation to frequency domain using fast fourier transformation
+
+from scipy import fftpack
+f = np.fft.fft2(img)
+fshift = np.fft.fftshift(f)
+
+#applying filter over the transformed image 
+fshift=np.float32(fshift)
+fshift = cv2.GaussianBlur(fshift,(5,5),0)
+
+#converting the transformed image to usable format(human understandable format)
+#To check whether the application of filter in frequency domain has any effect or not
+magnitude_spectrum = np.log(np.abs(fshift))
+temp=magnitude_spectrum*img
+kernel = np.array([[0,-1,0], 
+                   [-1, 12,-1],
+                   [0,-1,0]])
+sharpened = cv2.filter2D(temp, -1, kernel)
+
+#Plotting the image
+plt.subplot(131),plt.imshow(img, cmap = 'gray')
+plt.title('Input Image'), plt.xticks,k([]), plt.yticks([])
+plt.subplot(132),plt.imshow(magnitude_spectrum, cmap = 'gray')
+plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.subplot(133),plt.imshow(sharpened)
+plt.title('real'), plt.xticks([]), plt.yticks([])
+plt.show()
